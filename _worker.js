6ccd6d1ed3eeb6ac9d70b695463ce12b4cbb6f18
TheChuan1503 <104840099+TheChuan1503@users.noreply.github.com>
2024-06-23 20:26:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 export default {
     async fetch(request, env) {
         const _url = new URL(request.url);
-        _url.hostname = _url.pathname.startsWith("/ip/")
-        ? "ip138.com"
+        _url.hostname = _url.pathname.startsWith("/gh/")
+        ? "cdn.jsdelivr.net"
         : "ruijiaoyu.net";
         const req = new Request(_url, request);
         return fetch(req);
