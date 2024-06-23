7a765bb8fd3ec8d44e4bfe0f22555a1eb37c81c4
TheChuan1503 <2683390185@qq.com>
2024-06-23 13:21:13 +0800

    c

--- _worker.js
+++ _worker.js
@@ -1,10 +1,10 @@
 export default {
-async fetch(request, env) {
-const _url = new URL(request.url);
-_url.hostname = _url.pathname.startsWith("/gh/")
-? "cdn.jsdelivr.net"
-: "www.baidu.com";
-const req = new Request(_url, request);
-return fetch(req);
-},
+    async fetch(request, env) {
+        const _url = new URL(request.url);
+        _url.hostname = _url.pathname.startsWith("/ip/")
+        ? "ip138.com"
+        : "ruijiaoyu.net";
+        const req = new Request(_url, request);
+        return fetch(req);
+    },
 };
